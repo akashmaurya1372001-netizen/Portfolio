f04e977ea0d992b9496e2e5b55b14442a9837267
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-26 01:43:52 +0530

    About

---
+++ Portfolio/src/Components/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <section id="about" classname="p-20 text-center">
+      <h2>About me</h2>
+      <p className="max-w-xl mx-auto">
+        I 'm a react devloper passionate about building mordern wed appliction
+        using React ,tailwind and backend services like APPWRITE.
+      </p>
+    </section>
+  );
+}
+export default About;

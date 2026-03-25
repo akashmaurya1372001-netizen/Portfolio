c514b5316e822cd839c18b4460e3054fc55a4c0d
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-26 01:51:40 +0530

    project array

---
+++ Portfolio/src/Components/data/project.js
@@ -0,0 +1,13 @@
+const project=[
+    {
+        title:"Currency Converter",
+        tech:"React + API",
+        github:"#"
+    },
+    {
+        title:"Blog App",
+        tech: "React + Appwrite",
+        github:"#"
+    }
+];
+export default project;
